88481b563f853c3420833afc5fea0f3c09d7bce2
Zach Lipp <zlipp@protonmail.com>
2021-02-22 22:05:19 -0600

    combine scripts

--- snowpiles.sh
+++ snowpiles.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
-set -exuo pipefail
+set -uo pipefail
 
+# ---
+# Download snowfall maps
+# ---
+mkdir -p maps
+
+BASE_URL="http://www.nohrsc.noaa.gov/snowfall/data/"
+# `ls` outputs files alphabetically
+PAST=$(ls maps | tail -n 1)
+# Snowfall maps start on 09/30
+SNOWFALL_START="$(date +%Y -d $PAST)093012"
+
+until [[ $PAST == ${TODAY} ]]; do
+  BASENAME="sfav2_CONUS_${SNOWFALL_START}_to_$(date +%Y%m%d -d $PAST)12.tif"
+  YEARMON=$(date -d ${PAST} +%Y%m)
+  curl \
+    -L \
+    --silent \
+    -o maps/${PAST}.tif \
+    ${BASE_URL}/${YEARMON}/${BASENAME}
+  echo $BASENAME
+  # The NOAA are our friends
+  sleep 1
+  PAST=$(date -I -d "$PAST + 1 day")
+done
+
+# ---
+# Transformations
+# ---
 mkdir -p albers
 for map in maps/*.tif; do
   if [[ ! -f albers/$map ]]; then
